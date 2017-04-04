da9db9b7ccfe780fc01c95834165ea246d8b8a3f
riddhigala0910 <riddhi.gala0910@gmail.com>
2017-04-04 14:31:30 +0200

    Reverted and added

--- src/com/dynamic/sse/DSSE.java
+++ src/com/dynamic/sse/DSSE.java
@@ -10,24 +10,20 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.crypto.NoSuchPaddingException;
 
 import com.dynamic.sse.CryptoPrimitives;
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
 
 import textExtract.TextExtractPar;
 import textExtract.TextProc;
 
 public class DSSE {
 
-	static Multimap<String, String> words = ArrayListMultimap.create();
-	static Multimap<String, String> files = ArrayListMultimap.create();
 	static CryptoPrimitives cp = new CryptoPrimitives();
 	static List<byte[]> listSK = new ArrayList<byte[]>();
 
@@ -37,7 +33,7 @@ public class DSSE {
 	public static void main(String args[]) throws Exception {
 		BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in));
 		String path = null;
-		System.out.println("Enter folder path to index files");
+		System.out.println("Enter folder path to index files:");
 		
 		try 
 		{
@@ -50,26 +46,28 @@ public class DSSE {
 		
 		wordExtract(path);
 		listSK = cp.keyGen();
+		
 		DSSE dsse = new DSSE();
-		dsse.encryptFiles();
+		
+
+		Set<String> words = TextExtractPar.lp1.keySet();
+		Set<String> files = TextExtractPar.lp2.keySet();
+		
 		HashMap<String,ArrayList<Integer>> wordTable = new HashMap<String,ArrayList<Integer>>();
 		int i = 0;
-		for( Entry<String, Collection<String>> entry : words.asMap().entrySet())
+		for( String w: words)
 		{
-			Object key = entry.getKey();
-			String wordList = (new CryptoPrimitives()).generateCMAC(listSK.get(0), key.toString());
-			String words = wordList.toString();
-			ArrayList<Integer> state = new ArrayList<Integer>();
-			state.add(i++);
-			state.add(0);
-			wordTable.put(words, state);
+			ArrayList<Integer> temp = new ArrayList<Integer>();
+			temp.add(i++);
+			temp.add(0);
+			wordTable.put(cp.generateCMAC(listSK.get(0), w), temp);
 		}
-		for (Entry<String, ArrayList<Integer>> ee : wordTable.entrySet()) 
-		{
+		
+		for (Entry<String, ArrayList<Integer>> ee : wordTable.entrySet()) {
 		    String key = ee.getKey();
 		    List<Integer> values = ee.getValue();
 		    System.out.println("Hash Table for words");
-		    System.out.println(key + " :::::: " + values);
+		   System.out.println(key + " :::::: " + values);
 		}
 		
 		
@@ -77,32 +75,32 @@ public class DSSE {
 		
 		HashMap<String,ArrayList<Integer>> fileTable = new HashMap<String,ArrayList<Integer>>();
 		int j = 0;
-		for( Entry<String, Collection<String>> entry : files.asMap().entrySet())
+		for( String f: files)
 		{
-			Object key = entry.getKey();
-			String a = (new CryptoPrimitives()).generateCMAC(listSK.get(0), key.toString());
-			String s = a.toString();
 			ArrayList<Integer> temp = new ArrayList<Integer>();
 			temp.add(j++);
 			temp.add(0);
-			fileTable.put(s, temp);
+			fileTable.put(cp.generateCMAC(listSK.get(0), f), temp);
 		}
 		
 		for (Entry<String, ArrayList<Integer>> ee : fileTable.entrySet()) 
 		{
+			
 		    String key = ee.getKey();
 		    List<Integer> values = ee.getValue();
+		    
 		    System.out.println("Hash Table for files");
 		    System.out.println(key + " :::::: " + values);
 		}
 		
+		dsse.encryptFiles();
 		Matrix A = Matrix.random(wordTable.size(), fileTable.size());
 	}
 
 	private void encryptFiles() {
 		HashMap<String, Integer> fileMap = new HashMap<String, Integer>();
 		int count = 1;
-		for (String f : files.keySet()) {
+		for (String f : TextExtractPar.lp2.keySet()) {
 			String fileEncrypt = cp.encryptCCM(listSK.get(2), listSK.get(3), f);
 			fileMap.put(fileEncrypt, count);
 			count++;
@@ -114,8 +112,7 @@ public class DSSE {
 		TextProc.listf(filePath, listOfFiles);
 		try {
 			TextProc.TextProc(false, filePath);
-			words = TextExtractPar.lp1;
-			files = TextExtractPar.lp2;
+
 		} catch (InvalidKeyException | InvalidAlgorithmParameterException | NoSuchAlgorithmException
 				| NoSuchProviderException | NoSuchPaddingException | InvalidKeySpecException | IOException e) {
 			e.printStackTrace();
